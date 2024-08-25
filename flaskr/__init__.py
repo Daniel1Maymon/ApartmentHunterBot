1218a46d6e9903a4b3d1725c3e7af83a2b11d014
Daniel <daniel1maymon@gmail.com>
2024-08-25 16:06:47 +0300

    Set up Flask app with MongoDB integration
    - Initialized Flask app with environment-based MongoDB configuration.
    - Added error handling middleware for PyMongo exceptions.
    - Registered application routes and initialized the database connection.
    
    These changes establish the core setup for the Flask application, including database connectivity and error management

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
+from .database import init_app
+import os
+from dotenv import load_dotenv
+from etc.fb_scraper import  get_env_path
+from pymongo.errors import PyMongoError
+
+# Load the .env file
+load_dotenv(dotenv_path=get_env_path())
 
 def create_app():
     app = Flask(__name__)
+    
+    # Load configuration
+    app.config["MONGO_URI"] = os.getenv(key="MONGO_URL")
 
     # Register Blueprints or routes here
     from . import routes
     app.register_blueprint(blueprint=routes.bp)
 
+    # Initialize the database
+    init_app(app)
+    
+    # Middleware to handle PyMongo exceptions
+    @app.errorhandler(code_or_exception=PyMongoError)
+    def handle_pymongo_error(error):
+        app.logger.error(f"Database error: {error}")
+        return jsonify({"status": "error", "message": "A database error occurred."}), 500
+
     return app
+
