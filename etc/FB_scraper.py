271a9819a43b84d6cdc09824240414b61b6cd61b
Daniel <daniel1maymon@gmail.com>
2024-08-26 14:27:58 +0300

    Adding filters for check before saving post in the DB

--- etc/FB_scraper.py
+++ etc/FB_scraper.py
@@ -3,6 +3,57 @@ from dotenv import load_dotenv
 import os, time, random
 from flaskr.database import mongo
 
+group_links = ["https://www.facebook.com/groups/1380680752778760/?sorting_setting=CHRONOLOGICAL",
+               "https://www.facebook.com/groups/1870209196564360/?sorting_setting=CHRONOLOGICAL",
+               "https://www.facebook.com/groups/520940308003364/?sorting_setting=CHRONOLOGICAL",
+               "https://www.facebook.com/groups/647901439404148/?sorting_setting=CHRONOLOGICAL",
+               "https://www.facebook.com/groups/186810449287215/?sorting_setting=CHRONOLOGICAL",
+               "https://www.facebook.com/groups/2098391913533248/?sorting_setting=CHRONOLOGICAL",
+               "https://www.facebook.com/groups/1998122560446744/?sorting_setting=CHRONOLOGICAL",
+               "https://www.facebook.com/groups/1424244737803677/?sorting_setting=CHRONOLOGICAL",
+               "https://www.facebook.com/groups/1068642559922565/?sorting_setting=CHRONOLOGICAL",
+               "https://www.facebook.com/groups/1380680752778760/?sorting_setting=CHRONOLOGICAL",
+               "https://www.facebook.com/groups/654949551249110/?sorting_setting=CHRONOLOGICAL",
+               "https://www.facebook.com/groups/564985183576779/?sorting_setting=CHRONOLOGICAL"
+               ]
+
+filters = [
+    "רמת גן", 
+    'ר"ג', 
+    "רמת-גן", 
+    "שותף", 
+    "שותפה", 
+    "שותפ"
+    "סטודיו", 
+    "יחידת דיור",  
+    "4 חד",
+    "5 חד", 
+    "2 חד"
+    "מתפנה חדר", 
+    "הרצל", 
+    "ז'בוטינסקי", 
+    "ברמת גן", 
+    'אצ"ל',
+    "מרום נווה",
+    "מחליף",
+    "קונה ברזל",
+    "2 שותפים",
+    "3 שותפים",
+    "גלריה",
+    "ללא מקלט",
+    "אין מקלט",
+    'ללא בע',
+    "אסור בע",
+    "בת ים",
+    "פנטאוז",
+    "מחפש דירה",
+    "מחפש דירת",
+    "מחפשת דירה",
+    "מחפשת דירת",
+    "רמת חן",
+    "מתפנה חדר"
+]
+
 def get_env_path() -> str:
     # Get the directory of the current script
     current_directory = os.path.dirname(__file__)
@@ -88,6 +139,14 @@ def click_on_see_more_button(page, post):
     if see_more_button and see_more_button.is_visible():
         see_more_button.click()
         page.wait_for_timeout(1000)  # Wait for content to expand
+ 
+def post_contain_unwanted_words(post_content):
+    for word in filters:
+        if word in post_content:
+            print(f"------\nThe word [{word}] is in [{post_content}]\n-------\n")
+            return True
+        
+    return False
     
 def get_post_link(post):
     link_elements = post.query_selector_all("a[href]")    
@@ -131,20 +190,23 @@ def scrape_group_posts(page, group_url, max_posts=10):
             post_text = post.inner_text()
             post_link = get_post_link(post)
             post_url_id = post_link.split("/")[-1]
-            post_exists = mongo.db.collection.find_one({"link": {"$regex": post_url_id}})
+            post_link_exists = mongo.db.collection.find_one({"link": {"$regex": post_url_id}})
             
-            if len(post_text) > 0 and not post_exists:
-                print(":: post_content ::")
-                post_content = post.query_selector("div[data-ad-preview='message']")
-                if post_content:  
-                    post_content = post_content.inner_text()
-                    # posts.append([post_content, post_link])
-                    _post = {
-                        "link": post_link,
-                        "content": post_content,
-                        "hasBeenSent": False
-                    }
-                    posts.append(_post)
+            
+            if len(post_text) > 0 and not post_link_exists:    
+                post_content_element = post.query_selector("div[data-ad-preview='message']")
+                if post_content_element:  
+                    post_content = post_content_element.inner_text()
+                    print(f"---\npost_text[:10]= {post_text[:10]}")
+                    print(f"---\npost_text[:10]= {post_content[:10]}")
+                    post_content_exists = mongo.db.collection.find_one({"content": post_content})
+                    if not post_contain_unwanted_words(post_content) and not post_content_exists:
+                        _post = {
+                            "link": post_link,
+                            "content": post_content,
+                            "hasBeenSent": False
+                        }
+                        posts.append(_post)
                     
                 print(":: END OF post_content ::")
                 # posts.append(post_text)
@@ -169,21 +231,24 @@ def make_login_and_get_posts():
         login_to_facebook(page, username, password)
 
         group_url = "https://www.facebook.com/groups/647901439404148/?sorting_setting=CHRONOLOGICAL"  # Replace with your group URL
-        posts = scrape_group_posts(page, group_url)
-
+        posts = []
+        for link in group_links:
+            posts.extend(scrape_group_posts(page, link))
+            pass
+            
         print(f"Scraped {len(posts)} posts")
-        
         browser.close()
         
     return posts
     
 def run_scraper():
+    start_time = time.time()
     posts = make_login_and_get_posts() 
+    end_time = time.time()
+    total_time = end_time-start_time
+    print(f"\n---------\ntotal running time: {total_time} ({(total_time/60):.2f} minutes)\n---------\n")
     return posts
-    for post in posts:
-        post["hasBeenSent"] = False
-        mongo.db.posts.insert_one(post)
-        
+ 
 def main():
 
     # run_multiple_logins(1, username, password)
