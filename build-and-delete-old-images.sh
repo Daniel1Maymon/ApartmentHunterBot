1a9723e439290060b0376ac9454a5da9a572f77f
Daniel <daniel1maymon@gmail.com>
2024-09-17 15:18:58 +0300

    Create sh file to build and delete old images

---
+++ build-and-delete-old-images.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+docker stop $(docker ps -aq)
+
+docker rm -f $(docker ps -aq)
+
+# Find and remove all dangling images (images with <none> tag)
+DANGLING_IMAGES=$(docker images -f "dangling=true" -q)
+if [ -n "$DANGLING_IMAGES" ]; then
+    docker rmi -f $DANGLING_IMAGES
+fi
+
+# Build the new image
+docker build -f dockerfile -t apartment-hunter-bot .
+
+# Find and remove all dangling images (images with <none> tag)
+DANGLING_IMAGES=$(docker images -f "dangling=true" -q)
+if [ -n "$DANGLING_IMAGES" ]; then
+    docker rmi -f $DANGLING_IMAGES
+fi
