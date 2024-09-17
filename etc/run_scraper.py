f6eb62cd297de7d62d731b8d496ea3cf8b957915
Daniel <daniel1maymon@gmail.com>
2024-09-17 17:06:48 +0300

    Add time

--- etc/run_scraper.py
+++ etc/run_scraper.py
@@ -1,10 +1,13 @@
 import requests
+from datetime import datetime
 
 def run_scraper():
     response = requests.get('http://localhost:5000/run_scraper')
     print(f"Status code: {response.status_code}")
 
 if __name__ == '__main__':
+    current_time = datetime.now()
+    print(f"time: {current_time.strftime("%H:%M:%S")}")
     print("Scraper is running...")
     run_scraper()
     print("Scraper is finish...")
